e4720fb3994120b1fdea18ba5b1bc644a7bfe1e9
Russell Fustino <rfustino@hotmail.com>
2021-03-23 17:24:45 -0400

    DryrunDebugging UI and Tweaks

--- app/algorandapp/DryrunDebugging.xaml.cs
+++ app/algorandapp/DryrunDebugging.xaml.cs
@@ -11,75 +11,94 @@ using Algorand.Client;
 using Algorand.V2;
 using System.Diagnostics;
 using System.IO;
+using Newtonsoft.Json;
+//using Algorand.V2.Model;
+using Account = Algorand.Account;
 
 namespace algorandapp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DryrunDebugging : ContentPage
     {
+        static helper helper = new helper();
+        AlgodApi client;
+        Account account1;
+        Account account2;   
         public DryrunDebugging()
         {
-            // UI code to be completed - testing only
             InitializeComponent();
-            DryrunDebuggingExample();
+            Appearing += DryrunDebugging_Appearing;         
         }
 
-        void DryrunDebuggingExample()
+        private async void DryrunDebugging_Appearing(object sender, EventArgs e)
         {
+            client = await helper.CreateApiInstance();
+            // restore accounts
+            var accounts = await helper.RestoreAccounts();
+            account1 = accounts[0];
+            account2 = accounts[1];
 
-            string ALGOD_API_ADDR = "http://hackathon.algodev.network:9100";
-            string ALGOD_API_TOKEN = "ef920e2e7e002953f4b29a8af720efe8e4ecc75ff102b165e0472834b25832c1";
-
-            string SRC_ACCOUNT = "buzz genre work meat fame favorite rookie stay tennis demand panic busy hedgehog snow morning acquire ball grain grape member blur armor foil ability seminar";
-            Account acct1 = new Account(SRC_ACCOUNT);
-            var acct2Address = "QUDVUXBX4Q3Y2H5K2AG3QWEOMY374WO62YNJFFGUTMOJ7FB74CMBKY6LPQ";
-
-          //  byte[] source = File.ReadAllBytes("V2\\contract\\sample.teal");
-            byte[] source = ExtractResource("algorandapp.Contract.sample.teal");
-            byte[] program = Convert.FromBase64String("ASABASI=");
-
-            LogicsigSignature lsig = new LogicsigSignature(program, null);
-
-            // sign the logic signaure with an account sk
-            acct1.SignLogicsig(lsig);
-
-            var algodApiInstance = new AlgodApi(ALGOD_API_ADDR, ALGOD_API_TOKEN);
-            Algorand.V2.Model.TransactionParametersResponse transParams;
-            try
-            {
-                transParams = algodApiInstance.TransactionParams();
-            }
-            catch (ApiException e)
-            {
-                throw new Exception("Could not get params", e);
-            }
-
-            Transaction tx = Utils.GetPaymentTransaction(acct1.Address, new Address(acct2Address), 1000000,
-                "tx using in dryrun", transParams);
-
-            try
-            {
-                //bypass verify for non-lsig
-                SignedTransaction signedTx = Account.SignLogicsigTransaction(lsig, tx);
-                //一切准备就绪，本可以直接发送到网络，也可使得Dryrun的方法来进行调试
-                //var id = Utils.SubmitTransaction(algodApiInstance, signedTx);
-                //Console.WriteLine("Successfully sent tx logic sig tx id: " + id);
-                // dryrun source
-                var dryrunResponse = Utils.GetDryrunResponse(algodApiInstance, signedTx, source);
-                Debug.WriteLine("Dryrun compiled repsonse : " + dryrunResponse.ToJson()); // pretty print
-
-                // dryrun logic sig transaction
-                //var dryrunResponse2 = Utils.GetDryrunResponse(algodApiInstance, signedTx);
-                //Debug.WriteLine("Dryrun source repsonse : " + dryrunResponse2.ToJson()); // pretty print
-            }
-            catch (ApiException e)
-            {
-                // This is generally expected, but should give us an informative error message.
-                Debug.WriteLine("Exception when calling algod#rawTransaction: " + e.Message);
-            }
-
-
+            Console.WriteLine("Account 1: " + account1.Address);
+            Console.WriteLine("Account 2: " + account2.Address);
         }
+
+        //void DryrunDebuggingExample()
+        //{
+
+        //    string ALGOD_API_ADDR = "http://hackathon.algodev.network:9100";
+        //    string ALGOD_API_TOKEN = "ef920e2e7e002953f4b29a8af720efe8e4ecc75ff102b165e0472834b25832c1";
+
+        //    string SRC_ACCOUNT = "buzz genre work meat fame favorite rookie stay tennis demand panic busy hedgehog snow morning acquire ball grain grape member blur armor foil ability seminar";
+        //    Account acct1 = new Account(SRC_ACCOUNT);
+        //    var acct2Address = "QUDVUXBX4Q3Y2H5K2AG3QWEOMY374WO62YNJFFGUTMOJ7FB74CMBKY6LPQ";
+
+        //  //  byte[] source = File.ReadAllBytes("V2\\contract\\sample.teal");
+        //    byte[] source = ExtractResource("algorandapp.Contract.sample.teal");
+        //    byte[] program = Convert.FromBase64String("ASABASI=");
+
+        //    LogicsigSignature lsig = new LogicsigSignature(program, null);
+
+        //    // sign the logic signaure with an account sk
+        //    acct1.SignLogicsig(lsig);
+
+        //    var algodApiInstance = new AlgodApi(ALGOD_API_ADDR, ALGOD_API_TOKEN);
+        //    Algorand.V2.Model.TransactionParametersResponse transParams;
+        //    try
+        //    {
+        //        transParams = algodApiInstance.TransactionParams();
+        //    }
+        //    catch (ApiException e)
+        //    {
+        //        throw new Exception("Could not get params", e);
+        //    }
+
+        //    Transaction tx = Utils.GetPaymentTransaction(acct1.Address, new Address(acct2Address), 1000000,
+        //        "tx using in dryrun", transParams);
+
+        //    try
+        //    {
+        //        //bypass verify for non-lsig
+        //        SignedTransaction signedTx = Account.SignLogicsigTransaction(lsig, tx);
+        //        //一切准备就绪，本可以直接发送到网络，也可使得Dryrun的方法来进行调试
+        //        //var id = Utils.SubmitTransaction(algodApiInstance, signedTx);
+        //        //Console.WriteLine("Successfully sent tx logic sig tx id: " + id);
+        //        // dryrun source
+        //        var dryrunResponse = Utils.GetDryrunResponse(algodApiInstance, signedTx, source);
+        //        Debug.WriteLine("Dryrun compiled repsonse : " + dryrunResponse.ToJson()); // pretty print
+
+        //        // dryrun logic sig transaction
+        //        //var dryrunResponse2 = Utils.GetDryrunResponse(algodApiInstance, signedTx);
+        //        //Debug.WriteLine("Dryrun source repsonse : " + dryrunResponse2.ToJson()); // pretty print
+        //    }
+        //    catch (ApiException e)
+        //    {
+        //        // This is generally expected, but should give us an informative error message.
+        //        Debug.WriteLine("Exception when calling algod#rawTransaction: " + e.Message);
+        //    }
+
+
+        //}
+
         public static byte[] ExtractResource(String filename)
         {
             System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
@@ -90,6 +109,68 @@ namespace algorandapp
                 resFilestream.Read(ba, 0, ba.Length);
                 return ba;
             }
+        }
+
+        void DryrunDebugging_Clicked(System.Object sender, System.EventArgs e)
+        {
+            //Account acct1 = new Account(SRC_ACCOUNT);
+            //var acct2.Address = "QUDVUXBX4Q3Y2H5K2AG3QWEOMY374WO62YNJFFGUTMOJ7FB74CMBKY6LPQ";
+
+            //  byte[] source = File.ReadAllBytes("V2\\contract\\sample.teal");
+            byte[] source = ExtractResource("algorandapp.Contract.sample.teal");
+            byte[] program = Convert.FromBase64String("ASABASI=");
+
+            LogicsigSignature lsig = new LogicsigSignature(program, null);
+
+            // sign the logic signaure with an account sk
+            account1.SignLogicsig(lsig);
+
+         //   var algodApiInstance = new AlgodApi(ALGOD_API_ADDR, ALGOD_API_TOKEN);
+            Algorand.V2.Model.TransactionParametersResponse transParams;
+            try
+            {
+                transParams = client.TransactionParams();
+            }
+            catch (ApiException err)
+            {
+                throw new Exception("Could not get params", err);
+            }
+
+            Transaction tx = Utils.GetPaymentTransaction(account1.Address, account2.Address, 1000000,
+                "tx using in dryrun", transParams);
+
+            try
+            {
+                //bypass verify for non-lsig
+                SignedTransaction signedTx = Account.SignLogicsigTransaction(lsig, tx);
+
+
+                // dryrun source
+                var dryrunResponse = Utils.GetDryrunResponse(client, signedTx, source);
+                Debug.WriteLine("Dryrun compiled repsonse : " + dryrunResponse.ToJson()); // pretty print
+                DisplayInfo("Dryrun compiled repsonse : " + dryrunResponse.ToJson());
+                // dryrun logic sig transaction - omit source
+                //var dryrunResponse2 = Utils.GetDryrunResponse(algodApiInstance, signedTx);
+                //Debug.WriteLine("Dryrun source repsonse : " + dryrunResponse2.ToJson()); // pretty print
+
+                //var id = Utils.SubmitTransaction(algodApiInstance, signedTx);
+                //Console.WriteLine("Successfully sent tx logic sig tx id: " + id);
+            }
+            catch (ApiException err)
+            {
+                // This is generally expected, but should give us an informative error message.
+                Debug.WriteLine("Exception when calling algod#rawTransaction: " + err.Message);
+            }
+
+
+        }
+        void DisplayInfo(string text)
+        {
+         
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = @"<html><body><h2>" +
+                 text + "</h2></body></html>";
+            myWebView.Source = htmlSource;
         }
     }
 }
--- app/algorandapp/Rekey.xaml.cs
+++ app/algorandapp/Rekey.xaml.cs
@@ -47,90 +47,7 @@ namespace algorandapp
 
         }
 
-        //void RekeyExamples()
-        //{
-
-
-        //    ////string ALGOD_API_ADDR = "https://testnet-algorand.api.purestake.io/idx2";
-        //    ////string ALGOD_API_TOKEN = "GeHdp7CCGt7ApLuPNppXN4LtrW07Mm1kaFNJ5Ovr";
-        //    ////string ALGOD_API_ADDR = "http://localhost:4001";
-        //    ////string ALGOD_API_TOKEN = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        //    //string ALGOD_API_ADDR = "http://hackathon.algodev.network:9100";
-        //    //string ALGOD_API_TOKEN = "ef920e2e7e002953f4b29a8af720efe8e4ecc75ff102b165e0472834b25832c1";
-
-
-        //    //var account1_passphrase = "fringe model trophy claw stove perfect address market license abstract master slender choice around field embark sudden carbon exclude abuse square bulb front ability violin";
-        //    //var account2_passphrase = "impulse nation creek toy carpet amused dream can small long disorder source mail game category damp spread length cupboard theory either baby squeeze about orbit";
-        //    //var account3_passphrase = "fade exit sword someone lock minimum scout keen label dance jaguar select conduct luxury rose idea solid major solid lens globe agent assume abstract alien";
-
-
-        //    //var account1 = new Account(account1_passphrase);
-        //    //var account2 = new Account(account2_passphrase);
-        //    ////    private_key = mnemonic.to_private_key(account3_passphrase)
-        //    //var account3 = new Account(account3_passphrase);
-
-        //    Console.WriteLine(string.Format("Account 1 : {0}", account1.Address));
-        //    Console.WriteLine(string.Format("Account 2 : {0}", account2.Address));
-        //    Console.WriteLine(string.Format("Account 3 : {0}", account3.Address));
-
-        //    //Part 1
-        //    //build transaction
-
-        // //   AlgodApi algodApiInstance = new AlgodApi(ALGOD_API_ADDR, ALGOD_API_TOKEN);
-        //    var trans = client.TransactionParams();
-        //    Console.WriteLine("Lastround: " + trans.LastRound.ToString());
-
-
-        //    bool firstRun = false;
-
-        //    if (firstRun)
-        //    {
-        //        ulong? amount = 0;
-        //        //opt-in send tx to same address as sender and use 0 for amount w rekey account to account 1
-        //        var tx = Utils.GetPaymentTransaction(account3.Address, account3.Address, amount, "pay message", trans);
-        //        tx.RekeyTo = account1.Address;
-
-        //        var signedTx = account3.SignTransaction(tx);
-        //        // send the transaction to the network and
-        //        // wait for the transaction to be confirmed
-        //        try
-        //        {
-        //            var id = Utils.SubmitTransaction(client, signedTx);
-        //            Console.WriteLine("Transaction ID: " + id);
-        //            //waitForTransactionToComplete(algodApiInstance, signedTx.transactionID);
-        //            //Console.ReadKey();
-        //            Console.WriteLine("Confirmed Round is: " +
-        //                Utils.WaitTransactionToComplete(client, id.TxId).ConfirmedRound);
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            //e.printStackTrace();
-        //            Console.WriteLine(e.Message);
-        //            return;
-        //        }
-        //    }
-
-        //    var act = client.AccountInformation(account3.Address.ToString());
-        //    Console.WriteLine(act);
-
-        //    ulong? amount2 = 1000000;
-        //    var tx2 = Utils.GetPaymentTransaction(account3.Address, account2.Address, amount2, "pay message", trans);
-        //    tx2.RekeyTo = account1.Address;
-        //    var signedTx2 = account1.SignTransaction(tx2);
-        //    try
-        //    {
-        //        var id = Utils.SubmitTransaction(client, signedTx2);
-        //        Console.WriteLine("Transaction ID: " + id);
-        //        Console.WriteLine("Confirmed Round is: " +
-        //            Utils.WaitTransactionToComplete(client, id.TxId).ConfirmedRound);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e.Message);
-        //        return;
-        //    }
-
-        //}
+ 
 
         void Optin_Clicked(System.Object sender, System.EventArgs e)
         {
@@ -210,7 +127,6 @@ namespace algorandapp
             htmlSource.Html = @"<html><body><h2>" +
                  text + "</h2></body></html>";
             myWebView.Source = htmlSource;
-
         }
 
         void ResetBack_Clicked(System.Object sender, System.EventArgs e)
