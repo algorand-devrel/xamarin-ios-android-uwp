35493d3b7cc9a5e0a4a039fa91dd4aba6ba76809
Russell Fustino <rfustino@hotmail.com>
2021-03-23 20:28:03 -0400

    Tweaks on Teal code

--- app/algorandapp/CompileTeal.xaml.cs
+++ app/algorandapp/CompileTeal.xaml.cs
@@ -45,12 +45,6 @@ namespace algorandapp
 
         void CompileTeal_Clicked(System.Object sender, System.EventArgs e)
         {
-  
-          //network = await helper.GetNetwork();
-            //string ALGOD_API_ADDR = "http://hackathon.algodev.network:9100";
-            //string ALGOD_API_TOKEN = "ef920e2e7e002953f4b29a8af720efe8e4ecc75ff102b165e0472834b25832c1";
-            // AlgodApi algodApiInstance = new AlgodApi(ALGOD_API_ADDR, ALGOD_API_TOKEN);
-
             byte[] data = ExtractResource("algorandapp.Contract.sample.teal");
             var response = client.TealCompile(data);
             Debug.WriteLine("response: " + response);
@@ -68,3 +62,7 @@ namespace algorandapp
         }
     }
 }
+//network = await helper.GetNetwork();
+//string ALGOD_API_ADDR = "http://hackathon.algodev.network:9100";
+//string ALGOD_API_TOKEN = "ef920e2e7e002953f4b29a8af720efe8e4ecc75ff102b165e0472834b25832c1";
+// AlgodApi algodApiInstance = new AlgodApi(ALGOD_API_ADDR, ALGOD_API_TOKEN);
